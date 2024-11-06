8d37237c48af87347d2eac54bbff711a51fb80c6
Asror0816 <raxmonovasror75@gmail.com>
2024-11-06 01:26:42 -0800

    Add files via upload

---
+++ 1.2 (1).py
@@ -0,0 +1,10 @@
+a = int(input("Write a number a: "))
+b = int(input("Write a number b: "))
+z = a + b
+c = a * b
+k1 = a ** 2
+k2 = b ** 2
+print(f"Sum: {z}\n" +
+      f"Multiply: {c}\n" +
+      f"a^2: {k1}\n" +
+      f"b^2: {k2}\n")
