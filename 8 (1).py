765031743d6db6cde9858458b786826c1530924a
Asror0816 <raxmonovasror75@gmail.com>
2024-11-06 01:45:59 -0800

    Add files via upload

---
+++ 8 (1).py
@@ -0,0 +1,24 @@
+# 1
+def greet():
+    print('Hello World!')
+
+greet()
+
+# 2
+def calculate_are(length, weidth):
+    print('Area:', length * weidth)
+
+calculate_are(10, 20)
+
+# 3
+def multiply(a, b):
+    return a * b
+
+print(multiply(10, 20))
+
+# 4
+def add(a):
+    return a + 5
+
+print(add(10))
+
